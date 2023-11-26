d5509dd1d3cc325de41894294b3396445c25f0f7
Apollo Vavra <apollolvavra@gmail.com>
2023-11-25 18:20:26 -0600

    added basic three.js portfolio code

--- main.js
+++ main.js
@@ -1,1 +1,109 @@
+import "./css/style.css"
 
+import * as THREE from 'three';
+
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const renderer = new THREE.WebGLRenderer({
+    canvas: document.querySelector('#bg'),
+});
+
+renderer.setPixelRatio( window.devicePixelRatio );
+renderer.setSize( window.innerWidth, window.innerHeight );
+
+camera.position.setZ(30);
+
+renderer.render( scene, camera );
+
+const geometry = new THREE.TorusGeometry(10, 3, 16, 100)
+const material = new THREE.MeshStandardMaterial( {color: 0xFF6347} );
+const torus = new THREE.Mesh(geometry, material);
+
+scene.add(torus)
+
+const ambientLight = new THREE.AmbientLight(0xffffff)
+
+scene.add(ambientLight)
+
+
+const controls = new OrbitControls(camera, renderer.domElement);
+
+function addStar() {
+    const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+    const material = new THREE.MeshStandardMaterial({color: 0xfffff})
+    const star = new THREE.Mesh( geometry, material );
+
+    const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100));
+
+    star.position.set(x, y, z);
+    scene.add(star)
+}
+
+Array(250).fill().forEach(addStar)
+
+const backgroundTexture = new THREE.TextureLoader().load('space.jpeg');
+scene.background = backgroundTexture;
+
+const x = 0, y = 0;
+
+const heartShape = new THREE.Shape();
+
+heartShape.moveTo( x + 5, y + 5 );
+heartShape.bezierCurveTo( x + 5, y + 5, x + 4, y, x, y );
+heartShape.bezierCurveTo( x - 6, y, x - 6, y + 7,x - 6, y + 7 );
+heartShape.bezierCurveTo( x - 6, y + 11, x - 3, y + 15.4, x + 5, y + 19 );
+heartShape.bezierCurveTo( x + 12, y + 15.4, x + 16, y + 11, x + 16, y + 7 );
+heartShape.bezierCurveTo( x + 16, y + 7, x + 16, y, x + 10, y );
+heartShape.bezierCurveTo( x + 7, y, x + 5, y + 5, x + 5, y + 5 );
+
+const geometry = new THREE.ShapeGeometry( heartShape );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const mesh = new THREE.Mesh( geometry, material ) ;
+scene.add( mesh );
+
+const christmasTexture = new THREE.TextureLoader().load('christmas1.png');
+
+const christmas = new THREE.Mesh(
+    new THREE.SphereGeometry(3, 32, 32),
+    new THREE.MeshStandardMaterial({
+        map: christmasTexture,
+    })
+);
+
+scene.add(christmas);
+
+
+christmas.position.z = 30;
+
+function moveCamera() {
+    const t = document.body.getBoundingClientRect().top;
+    christmas.rotation.x += 0.05;
+    christmas.rotation.y += 0.075;
+    christmas.rotation.z += 0.05;
+
+    me.rotation.y += 0.01;
+    me.rotation.z += 0.01;
+
+    camera.position.z = t * -0.01;
+    camera.position.x = t * -0.0002;
+    camera.rotation.y = t * -0.0002;
+}
+
+document.body.onscroll = moveCamera
+
+
+function animate(){
+    requestAnimationFrame(animate);
+
+    torus.rotation.x += 0.01;
+    torus.rotation.y += 0.005
+    torus.rotation.z += 0.01;
+
+    renderer.render(scene, camera);
+}
+
+animate()
